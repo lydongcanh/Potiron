59b12c6c8444fc1e412fd9732f489a0430ed47e7
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-01-29 21:19:53 +0700

    Install react navigation.

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createMaterialTopTabNavigator } from "react-navigation-tabs";
+import HomeScreen from './src/components/screens/HomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const StackNavigator = createMaterialTopTabNavigator({
+        Home: {
+            screen: HomeScreen, 
+            navigationOptions: { title: "Home" }
+        }
+    });
+    
+    const AppContainer = createAppContainer(StackNavigator);
+
+    return <AppContainer />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
